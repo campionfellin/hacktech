89a26787eb7af7fd2ec4ff10c8cda7fed4722a80
Campion Fellin <campionfellin@gmail.com>
2017-03-04 10:42:48 -0800

    fix port

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.get('/control', function(request, response) {
 
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Node app is running on port', 3000);
 });
 
 function start() {
